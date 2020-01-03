cf4080751c2be88fb9ba1a28bab42f87d6cb12c2
pedro-yampi <pedro@yampi.com.br>
2020-01-03 17:12:07 -0300

    fazendo ajustes

--- src/pages/Default.vue
+++ src/pages/Default.vue
@@ -3,6 +3,9 @@
         <top-bar/>
         <div class="content container">
             <router-view :url="baseURL"/>
+            <div class="bg-overlay" v-show="isVisible" @click="hideOverlay">
+                <div class="spinner"></div>
+            </div>
         </div>
         
     </div>
@@ -15,9 +18,24 @@ import TopBar from '@/components/TopBar'
 export default {
     name: 'Default',
     components: {TopBar},
+    computed: {
+        isVisible() {
+            return this.$store.state.isVisible
+        }
+    },
     data() {
         return {
             baseURL: 'https://pokeapi.co/api/v2/'
+        }
+    },
+    methods: {
+        showOverlay: function() {
+            this.$store.commit('show');
+            console.log('showing');
+        },
+        hideOverlay: function() {
+            this.$store.commit('hide');
+            console.log('hiding');
         }
     }
 }
@@ -27,5 +45,22 @@ export default {
     .content {
         padding-top: 40px;
     }
-
+    .bg-overlay {
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        opacity: 0.7;
+        background: $black;
+        .spinner {
+            background: url('../assets/pokeball-transparent.webp') no-repeat center/20%;
+            animation: spin 800ms linear infinite;
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+    }
 </style>
